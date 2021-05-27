04fb7732e4d95d7ede1c630a4d856b714ceed4b7
Your Name <you@example.com>
2021-05-27 17:36:35 +0900

    Fourth Commit

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app.debug = True
 
 @app.route('/', methods = ['GET', 'POST'])
 def hello_world():
-    return render_template('home.html' , data = "김태경")
+    return render_template('home.html')
+
+@app.route('/about', methods = ['GET', 'POST'])
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(port = 5000)
