814ccf1ab37bd26cdcf0789041f77af18f258a03
Your Name <you@example.com>
2021-05-27 16:55:25 +0900

    Third Commit

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask.templating import render_template
 
 app = Flask(__name__)
+app.debug = True
 
-
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def hello_world():
-    return 'Hello World!'
+    return render_template('home.html' , data = "김태경")
 
 if __name__ == '__main__':
-    app.run(port = 5000, debug = True)
+    app.run(port = 5000)
