77b26c3a05987ff55a0f2122954ee2a6f7c1d3d0
Your Name <you@example.com>
2021-05-28 11:09:41 +0900

    articles html

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.templating import render_template
+from data import Articles
 
 app = Flask(__name__)
 app.debug = True
@@ -11,6 +12,11 @@ def hello_world():
 @app.route('/about', methods = ['GET', 'POST'])
 def about():
     return render_template('about.html')
+
+@app.route('/articles', methods = ['GET', 'POST'])
+def articles():
+    articles = Articles()
+    return render_template('articles.html', articles = articles)
 
 if __name__ == '__main__':
     app.run(port = 5000)
